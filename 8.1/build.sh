6778a53224319ff6dd808445f0a760ef73112f2f
Ryan C. Durham <ryan@stagerightlabs.com>
2021-12-21 14:22:26 -0800

    Switch 8.1 image to use php image base and add PCOV

--- 8.1/build.sh
+++ 8.1/build.sh
@@ -2,4 +2,8 @@
 
 # Build and tag the PHP 8.1 image
 docker build . -t stagerightlabs/php-test-runner:8.1
-echo "The PHP 8.1 image has been built and tagged."
+
+if [ $? -eq 0 ]
+then
+    echo "The PHP 8.1 image has been built and tagged."
+fi
